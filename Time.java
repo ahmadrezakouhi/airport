647c8e5fef17b7823e9ef9fc674d4261349e6b97
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2021-01-30 15:05:32 +0330

    added Time class

---
+++ Time.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package airport;
+
+/**
+ *
+ * @author homa
+ */
+public class Time {
+
+    private int hour;
+    private int minutes;
+
+    public Time(int hour, int minutes) {
+        this.hour = hour;
+        this.minutes = minutes;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
+    }
+
+}
